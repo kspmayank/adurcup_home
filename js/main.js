c57c37029c29ac059a9be4bfacf224559c87b543
Mayank <mayankkashyap95@gmail.com>
2016-05-10 15:03:25 +0530

    Added Touch Support

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready( function() {
+
+	//View More Btn Function
 	$("#package1-btn").click(function () {
 		console.log("clicked");
 	    $("#package1-hide").css("display", "block");
@@ -10,9 +12,34 @@ $(document).ready( function() {
 	    $("#package2-btn").css("display", "none");
 	});
 
+	//Restart Animation on Slide Change
 	$(".slide-changer").click(function () {
 		$(".pro-barr").removeClass('pro-bar').animate({'nothing':null}, 1, function () {
 			$(this).addClass('pro-bar');
 		});
 	});
+
+	//Adding touch to carousel
+	$("#carousel-example-generic").swiperight(function() {  
+		$(this).carousel('prev');
+		$(".pro-barr").removeClass('pro-bar').animate({'nothing':null}, 1, function () {
+			$(this).addClass('pro-bar');
+		});
+	});  
+	$("#carousel-example-generic").swipeleft(function() {  
+		$(this).carousel('next');
+		$(".pro-barr").removeClass('pro-bar').animate({'nothing':null}, 1, function () {
+			$(this).addClass('pro-bar');
+		});
+	});  
+
+
+	//Adding Touch to Top Selling Carousel
+	$("#carousel-top-selling").swiperight(function() {  
+		$(this).carousel('prev');
+	});  
+	$("#carousel-top-selling").swipeleft(function() {  
+		$(this).carousel('next');
+	});  
+
 });
